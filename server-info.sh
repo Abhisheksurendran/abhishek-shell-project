ce8733656d0629e577b3e52fe96c39c65a15ca3f
Ubuntu <ubuntu@ip-172-31-37-231.ap-southeast-1.compute.internal>
2026-08-13 09:17:38 +0000

    Add server information script

---
+++ server-info.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "===== SERVER INFORMATION====="
+
+echo "Host Name:"
+
+hostname
+
+echo "Current User:"
+
+whoami
+
+echo "Current Directory:"
+
+pwd
+
+echo "DISK Usage:"
+
+df -h /
+
+echo "Memory Usage:"
+
+free -h 
+
+echo "=======  END ======"
